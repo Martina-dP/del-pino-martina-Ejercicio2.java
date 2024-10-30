f31b01d3b9eff87d9113881737961a67eda79af3
Martina del Pino <martudelpino@gmail.com>
2024-10-30 12:06:25 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int col = 0;
+        int row = 0;
+        int[][] boradChess = new int[row][col];
+
+        System.out.println(Arrays.deepToString(boradChess));
     }
 }
