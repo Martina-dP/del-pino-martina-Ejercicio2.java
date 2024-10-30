0fc043a59d39c4fb8ae56e5e4bb2f859d54f4464
Martina del Pino <martudelpino@gmail.com>
2024-10-30 14:30:11 +0100

    queries to users and base boradGame

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        int col = 0;
-        int row = 0;
-        int[][] boradChess = new int[row][col];
+        int boardGame = 8;
+        int boxSize ;
+        char pattern ;
+        Scanner input = new Scanner(System.in);
 
-        System.out.println(Arrays.deepToString(boradChess));
+        System.out.print("Introduce el carácter que usar como patrón: ");
+        pattern = input.next().charAt(0);
+
+        while (true){
+            System.out.print("Introduce  el tamaño de la casilla: ");
+            boxSize = input.nextInt();
+            if (boxSize >= 1 && boxSize <=15 ) break;
+            else System.out.println("Debes introducir un valor mayor o igual a 1 y menor o igual a 15");
+        }
+
+        for (int i = 0 ; i < boardGame; i++){
+            for (int m = 0 ; m < boardGame; m++){
+
+
+            }
+            System.out.println();
+        }
+
     }
 }
